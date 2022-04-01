a3e4a9b78b57845344efb050cf3ae709f106f5ca
greg <gabriel.gregorio1987@gmail.com>
2022-04-01 20:09:29 -0300

    Criando o serviço ProdutoServiceImpl

---
+++ src/main/java/br/com/turorial/estudos/vendas/domain/rest/service/impl/ProdutoServiceImpl.java
@@ -0,0 +1,13 @@
+package br.com.turorial.estudos.vendas.domain.rest.service.impl;
+
+import br.com.turorial.estudos.vendas.domain.entity.Produto;
+import br.com.turorial.estudos.vendas.domain.rest.dto.ProdutoDTO;
+import br.com.turorial.estudos.vendas.domain.rest.service.interfaces.ProdutoService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProdutoServiceImpl implements ProdutoService {
+
+
+
+}
