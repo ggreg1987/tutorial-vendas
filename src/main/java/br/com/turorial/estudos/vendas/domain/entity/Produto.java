d97d8515f308c152329e39a140698f6948b991df
greg <gabriel.gregorio1987@gmail.com>
2022-04-01 15:02:47 -0300

    Criando a entidade Produto

---
+++ src/main/java/br/com/turorial/estudos/vendas/domain/entity/Produto.java
@@ -0,0 +1,27 @@
+package br.com.turorial.estudos.vendas.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String descricao;
+    private BigDecimal precoUnitario;
+}
