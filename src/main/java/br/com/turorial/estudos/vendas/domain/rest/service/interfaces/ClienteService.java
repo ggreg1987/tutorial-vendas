626f73ad36aa622216b960e59c7a21693c9027ac
greg <gabriel.gregorio1987@gmail.com>
2022-04-01 16:28:41 -0300

    Criando assinatura para pesquisar todos

--- src/main/java/br/com/turorial/estudos/vendas/domain/rest/service/interfaces/ClienteService.java
+++ src/main/java/br/com/turorial/estudos/vendas/domain/rest/service/interfaces/ClienteService.java
@@ -3,7 +3,10 @@ package br.com.turorial.estudos.vendas.domain.rest.service.interfaces;
 import br.com.turorial.estudos.vendas.domain.entity.Cliente;
 import br.com.turorial.estudos.vendas.domain.rest.dto.ClienteDTO;
 
+import java.util.List;
+
 public interface ClienteService {
     Cliente salvar(ClienteDTO dto);
     Cliente pesquisarId(Long id);
+    List<Cliente> pesquisarTodos(Cliente filtro);
 }
