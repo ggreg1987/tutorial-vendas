745a0fa9a84a556f13c1ae947376280aedcab43c
greg <gabriel.gregorio1987@gmail.com>
2022-04-01 16:21:06 -0300

    Criando uma exception customizada de NOTFOUND

---
+++ src/main/java/br/com/turorial/estudos/vendas/exception/RegraNotFoundException.java
@@ -0,0 +1,8 @@
+package br.com.turorial.estudos.vendas.exception;
+
+public class RegraNotFoundException extends RuntimeException {
+    
+    public RegraNotFoundException(String message) {
+        super(message);
+    }
+}
