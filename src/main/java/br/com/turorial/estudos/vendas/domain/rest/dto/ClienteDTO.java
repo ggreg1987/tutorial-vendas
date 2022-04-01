89cdd514ea74487a0de45354cf2b7a3c54a83d29
greg <gabriel.gregorio1987@gmail.com>
2022-04-01 15:37:42 -0300

    Adicionando Builder,Noargs e Allargs no dto ClienteDTO

--- src/main/java/br/com/turorial/estudos/vendas/domain/rest/dto/ClienteDTO.java
+++ src/main/java/br/com/turorial/estudos/vendas/domain/rest/dto/ClienteDTO.java
@@ -1,8 +1,14 @@
 package br.com.turorial.estudos.vendas.domain.rest.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ClienteDTO {
 
     private Long cliente;
