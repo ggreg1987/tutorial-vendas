da753a2ec64bbef413bd9cc587ddd6e00c8b3c25
greg <gabriel.gregorio1987@gmail.com>
2022-04-01 20:10:12 -0300

    Importando o repositório de Produto

--- src/main/java/br/com/turorial/estudos/vendas/domain/rest/service/impl/ProdutoServiceImpl.java
+++ src/main/java/br/com/turorial/estudos/vendas/domain/rest/service/impl/ProdutoServiceImpl.java
@@ -1,13 +1,16 @@
 package br.com.turorial.estudos.vendas.domain.rest.service.impl;
 
 import br.com.turorial.estudos.vendas.domain.entity.Produto;
+import br.com.turorial.estudos.vendas.domain.repository.ProdutoRepository;
 import br.com.turorial.estudos.vendas.domain.rest.dto.ProdutoDTO;
 import br.com.turorial.estudos.vendas.domain.rest.service.interfaces.ProdutoService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ProdutoServiceImpl implements ProdutoService {
 
-
+    private final ProdutoRepository produtoRepository;
 
 }
