68e7007858fe177a4d24e9fad8b00906c803f6bf
greg <gabriel.gregorio1987@gmail.com>
2022-04-01 20:16:29 -0300

    Criando a interface pesquisar produto

--- src/main/java/br/com/turorial/estudos/vendas/domain/rest/service/interfaces/ProdutoService.java
+++ src/main/java/br/com/turorial/estudos/vendas/domain/rest/service/interfaces/ProdutoService.java
@@ -3,8 +3,11 @@ package br.com.turorial.estudos.vendas.domain.rest.service.interfaces;
 import br.com.turorial.estudos.vendas.domain.entity.Produto;
 import br.com.turorial.estudos.vendas.domain.rest.dto.ProdutoDTO;
 
+import java.util.List;
+
 public interface ProdutoService {
 
     Produto salvar(ProdutoDTO dto);
     Produto pesquisarId(Long id);
+    List<Produto> pesquisar(Produto filtro);
 }
