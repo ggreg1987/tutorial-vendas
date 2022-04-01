00cd0172055edbb4068a5e87cbbebd8705270386
greg <gabriel.gregorio1987@gmail.com>
2022-04-01 17:06:10 -0300

    Definindo como uma api-restful e mapeando para api/cliente

--- src/main/java/br/com/turorial/estudos/vendas/domain/rest/controller/ClienteController.java
+++ src/main/java/br/com/turorial/estudos/vendas/domain/rest/controller/ClienteController.java
@@ -1,4 +1,9 @@
 package br.com.turorial.estudos.vendas.domain.rest.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/cliente")
 public class ClienteController {
 }
