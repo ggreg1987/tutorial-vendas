20da9fc770114ab89f3510ee13d6e831b3db3c58
greg <gabriel.gregorio1987@gmail.com>
2022-04-01 17:07:38 -0300

    Anotando com RequiredArgsContructor e importando o ClienteService

--- src/main/java/br/com/turorial/estudos/vendas/domain/rest/controller/ClienteController.java
+++ src/main/java/br/com/turorial/estudos/vendas/domain/rest/controller/ClienteController.java
@@ -1,9 +1,14 @@
 package br.com.turorial.estudos.vendas.domain.rest.controller;
 
+import br.com.turorial.estudos.vendas.domain.rest.service.interfaces.ClienteService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/cliente")
+@RequiredArgsConstructor
 public class ClienteController {
+
+    private final ClienteService service;
 }
