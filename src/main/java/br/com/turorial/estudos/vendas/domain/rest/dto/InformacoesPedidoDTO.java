26a341d01f722742387cc857b19d1d28fc4863e6
greg <gabriel.gregorio1987@gmail.com>
2022-04-01 15:47:10 -0300

    Criando dto InformacoesPedidoDTO

---
+++ src/main/java/br/com/turorial/estudos/vendas/domain/rest/dto/InformacoesPedidoDTO.java
@@ -0,0 +1,24 @@
+package br.com.turorial.estudos.vendas.domain.rest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class InformacoesPedidoDTO {
+
+    private Long pedidoId;
+    private String nomeCliente;
+    private String cpfCliente;
+    private String dataPedido;
+    private BigDecimal total;
+    private String status;
+    private List<InformacoesItensPedidoDTO> itensCliente;
+
+}
