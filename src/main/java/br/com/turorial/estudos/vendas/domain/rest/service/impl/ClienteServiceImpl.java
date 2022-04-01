6012b691c66ea9e481721404accdedd1d660bfaa
greg <gabriel.gregorio1987@gmail.com>
2022-04-01 16:00:06 -0300

    importando o repositório de Cliente

--- src/main/java/br/com/turorial/estudos/vendas/domain/rest/service/impl/ClienteServiceImpl.java
+++ src/main/java/br/com/turorial/estudos/vendas/domain/rest/service/impl/ClienteServiceImpl.java
@@ -1,12 +1,17 @@
 package br.com.turorial.estudos.vendas.domain.rest.service.impl;
 
 import br.com.turorial.estudos.vendas.domain.entity.Cliente;
+import br.com.turorial.estudos.vendas.domain.repository.ClienteRepository;
 import br.com.turorial.estudos.vendas.domain.rest.dto.ClienteDTO;
 import br.com.turorial.estudos.vendas.domain.rest.service.interfaces.ClienteService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClienteServiceImpl implements ClienteService {
+
+    private ClienteRepository clienteRepository;
+
+
     @Override
     public Cliente salvar(ClienteDTO dto) {
         return null;
