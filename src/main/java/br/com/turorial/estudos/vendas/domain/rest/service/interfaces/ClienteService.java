507d51350d7f340ce9b51b6afdfbd8527e1f20e4
greg <gabriel.gregorio1987@gmail.com>
2022-04-01 15:55:24 -0300

    Criando uma assinatura salvar

--- src/main/java/br/com/turorial/estudos/vendas/domain/rest/service/interfaces/ClienteService.java
+++ src/main/java/br/com/turorial/estudos/vendas/domain/rest/service/interfaces/ClienteService.java
@@ -1,5 +1,8 @@
 package br.com.turorial.estudos.vendas.domain.rest.service.interfaces;
 
+import br.com.turorial.estudos.vendas.domain.entity.Cliente;
+import br.com.turorial.estudos.vendas.domain.rest.dto.ClienteDTO;
+
 public interface ClienteService {
-    
+    Cliente salvar(ClienteDTO dto);
 }
