61e40d3ebd2ab65d6301352bf343fc3fcfd19591
greg <gabriel.gregorio1987@gmail.com>
2022-04-01 15:42:56 -0300

    Criando dto ProdutoDTO

---
+++ src/main/java/br/com/turorial/estudos/vendas/domain/rest/dto/ProdutoDTO.java
@@ -0,0 +1,20 @@
+package br.com.turorial.estudos.vendas.domain.rest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProdutoDTO {
+
+    private Long produto;
+
+    private String descricao;
+    private BigDecimal precoUnitario;
+}
