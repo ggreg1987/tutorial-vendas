9ae7a0799b7df7c9368609fe650b92324495b150
greg <gabriel.gregorio1987@gmail.com>
2022-04-01 17:09:57 -0300

    Criando o endpoint salvar ,como PostMapping e status Created

--- src/main/java/br/com/turorial/estudos/vendas/domain/rest/controller/ClienteController.java
+++ src/main/java/br/com/turorial/estudos/vendas/domain/rest/controller/ClienteController.java
@@ -1,9 +1,12 @@
 package br.com.turorial.estudos.vendas.domain.rest.controller;
 
+import br.com.turorial.estudos.vendas.domain.entity.Cliente;
+import br.com.turorial.estudos.vendas.domain.rest.dto.ClienteDTO;
 import br.com.turorial.estudos.vendas.domain.rest.service.interfaces.ClienteService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @RequestMapping("api/cliente")
@@ -11,4 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class ClienteController {
 
     private final ClienteService service;
+
+    @PostMapping
+    @ResponseStatus(CREATED)
+    Cliente salvar(@RequestBody ClienteDTO dto) {
+        return service.salvar(dto);
+    }
 }
