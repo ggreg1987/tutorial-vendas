473d355d31915222ba2f1a7e9473f3e0e339809f
greg <gabriel.gregorio1987@gmail.com>
2022-04-02 20:34:58 -0300

    Criando o serviço InformacaoPedidoImpl e implementando a interface InformacaoPedidoService

---
+++ src/main/java/br/com/turorial/estudos/vendas/domain/rest/service/impl/InformacaoPedidoImpl.java
@@ -0,0 +1,14 @@
+package br.com.turorial.estudos.vendas.domain.rest.service.impl;
+
+import br.com.turorial.estudos.vendas.domain.rest.dto.InformacoesPedidoDTO;
+import br.com.turorial.estudos.vendas.domain.rest.service.interfaces.InformacaoPedidoService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InformacaoPedidoImpl implements InformacaoPedidoService {
+
+    @Override
+    public InformacoesPedidoDTO informacaoPedidoCompleto(Long id) {
+        return null;
+    }
+}
