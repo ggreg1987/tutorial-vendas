a63726b77bb77fe62a43e114ee1d3bdbf4859522
greg <gabriel.gregorio1987@gmail.com>
2022-04-02 20:44:27 -0300

    Criando um método privado para pegar os itens primeiro

--- src/main/java/br/com/turorial/estudos/vendas/domain/rest/service/impl/InformacaoPedidoImpl.java
+++ src/main/java/br/com/turorial/estudos/vendas/domain/rest/service/impl/InformacaoPedidoImpl.java
@@ -1,8 +1,12 @@
 package br.com.turorial.estudos.vendas.domain.rest.service.impl;
 
+import br.com.turorial.estudos.vendas.domain.entity.ItemPedido;
+import br.com.turorial.estudos.vendas.domain.rest.dto.InformacoesItensPedidoDTO;
 import br.com.turorial.estudos.vendas.domain.rest.dto.InformacoesPedidoDTO;
 import br.com.turorial.estudos.vendas.domain.rest.service.interfaces.InformacaoPedidoService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class InformacaoPedidoImpl implements InformacaoPedidoService {
@@ -10,5 +14,9 @@ public class InformacaoPedidoImpl implements InformacaoPedidoService {
     @Override
     public InformacoesPedidoDTO informacaoPedidoCompleto(Long id) {
         return null;
+    }
+
+    private List<InformacoesItensPedidoDTO> items(List<ItemPedido> itens) {
+
     }
 }
