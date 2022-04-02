2957c4b5f1b7b9c1aa3e655f7fa2f37073000687
greg <gabriel.gregorio1987@gmail.com>
2022-04-02 15:15:17 -0300

    Importando o repositório do ItemPedido

--- src/main/java/br/com/turorial/estudos/vendas/domain/rest/service/impl/PedidoServiceImpl.java
+++ src/main/java/br/com/turorial/estudos/vendas/domain/rest/service/impl/PedidoServiceImpl.java
@@ -3,6 +3,7 @@ package br.com.turorial.estudos.vendas.domain.rest.service.impl;
 import br.com.turorial.estudos.vendas.domain.entity.ItemPedido;
 import br.com.turorial.estudos.vendas.domain.entity.Pedido;
 import br.com.turorial.estudos.vendas.domain.repository.ClienteRepository;
+import br.com.turorial.estudos.vendas.domain.repository.ItempedidoRepository;
 import br.com.turorial.estudos.vendas.domain.repository.PedidoRepository;
 import br.com.turorial.estudos.vendas.domain.repository.ProdutoRepository;
 import br.com.turorial.estudos.vendas.domain.rest.dto.ItemPedidoDTO;
@@ -20,6 +21,7 @@ public class PedidoServiceImpl implements PedidoService {
     private final PedidoRepository pedidoRepository;
     private final ProdutoRepository produtoRepository;
     private final ClienteRepository clienteRepository;
+    private final ItempedidoRepository itempedidoRepository;
 
     @Override
     public Pedido salvar(PedidoDTO dto) {
