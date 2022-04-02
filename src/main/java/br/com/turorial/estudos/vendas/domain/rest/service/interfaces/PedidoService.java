2bad3ee92a9b64d9af44370d61d745cbc9ad5711
greg <gabriel.gregorio1987@gmail.com>
2022-04-02 15:07:37 -0300

    Criando a interface de salvar um pedido

--- src/main/java/br/com/turorial/estudos/vendas/domain/rest/service/interfaces/PedidoService.java
+++ src/main/java/br/com/turorial/estudos/vendas/domain/rest/service/interfaces/PedidoService.java
@@ -1,4 +1,9 @@
 package br.com.turorial.estudos.vendas.domain.rest.service.interfaces;
 
+import br.com.turorial.estudos.vendas.domain.entity.Pedido;
+import br.com.turorial.estudos.vendas.domain.rest.dto.PedidoDTO;
+
 public interface PedidoService {
+
+    Pedido salvar(PedidoDTO dto);
 }
