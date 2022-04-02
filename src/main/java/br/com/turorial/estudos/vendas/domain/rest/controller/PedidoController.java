648ab242f9fd04407266c8168ec67a2459bd8131
greg <gabriel.gregorio1987@gmail.com>
2022-04-02 16:24:59 -0300

    Importando a interface do Pedido

--- src/main/java/br/com/turorial/estudos/vendas/domain/rest/controller/PedidoController.java
+++ src/main/java/br/com/turorial/estudos/vendas/domain/rest/controller/PedidoController.java
@@ -1,11 +1,16 @@
 package br.com.turorial.estudos.vendas.domain.rest.controller;
 
+import br.com.turorial.estudos.vendas.domain.rest.service.interfaces.PedidoService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/pedido")
+@RequiredArgsConstructor
 public class PedidoController {
+
+    private final PedidoService service;
 
     
 }
