b8a9a3c0bd9f32496597c31cfc7f6cf7ac39e041
greg <gabriel.gregorio1987@gmail.com>
2022-04-02 15:11:46 -0300

    Criando um método privado para pegar os itens antes de salvar o Pedido

--- src/main/java/br/com/turorial/estudos/vendas/domain/rest/service/impl/PedidoServiceImpl.java
+++ src/main/java/br/com/turorial/estudos/vendas/domain/rest/service/impl/PedidoServiceImpl.java
@@ -1,16 +1,24 @@
 package br.com.turorial.estudos.vendas.domain.rest.service.impl;
 
+import br.com.turorial.estudos.vendas.domain.entity.ItemPedido;
 import br.com.turorial.estudos.vendas.domain.entity.Pedido;
+import br.com.turorial.estudos.vendas.domain.rest.dto.ItemPedidoDTO;
 import br.com.turorial.estudos.vendas.domain.rest.dto.PedidoDTO;
 import br.com.turorial.estudos.vendas.domain.rest.service.interfaces.PedidoService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PedidoServiceImpl implements PedidoService {
 
-    
+
     @Override
     public Pedido salvar(PedidoDTO dto) {
         return null;
+    }
+
+    private List<ItemPedido> items(Pedido pedido, ItemPedidoDTO itens) {
+        
     }
 }
