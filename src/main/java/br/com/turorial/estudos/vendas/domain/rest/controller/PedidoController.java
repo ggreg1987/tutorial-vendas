da3ad0748b534d499688b3671ab5c8a45b0ee251
greg <gabriel.gregorio1987@gmail.com>
2022-04-02 16:28:20 -0300

    Criando endpoint salvar Pedido e retornando o Id para ser usado no InformacoesPedidoDTO

--- src/main/java/br/com/turorial/estudos/vendas/domain/rest/controller/PedidoController.java
+++ src/main/java/br/com/turorial/estudos/vendas/domain/rest/controller/PedidoController.java
@@ -1,9 +1,12 @@
 package br.com.turorial.estudos.vendas.domain.rest.controller;
 
+import br.com.turorial.estudos.vendas.domain.entity.Pedido;
+import br.com.turorial.estudos.vendas.domain.rest.dto.PedidoDTO;
 import br.com.turorial.estudos.vendas.domain.rest.service.interfaces.PedidoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.*;
 
 @RestController
 @RequestMapping("api/pedido")
@@ -12,5 +15,10 @@ public class PedidoController {
 
     private final PedidoService service;
 
-    
+    @PostMapping
+    @ResponseStatus(CREATED)
+    public Long salvar(@RequestBody PedidoDTO dto) {
+        Pedido pedido = service.salvar(dto);
+        return pedido.getId();
+    }
 }
