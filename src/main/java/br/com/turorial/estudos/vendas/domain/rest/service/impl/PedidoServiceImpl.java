314255832c839288e0ddda8533748793a6a843f4
greg <gabriel.gregorio1987@gmail.com>
2022-04-02 15:40:52 -0300

    Alterando a variável multiplicar para quantidadeBig para melhor entendimento

--- src/main/java/br/com/turorial/estudos/vendas/domain/rest/service/impl/PedidoServiceImpl.java
+++ src/main/java/br/com/turorial/estudos/vendas/domain/rest/service/impl/PedidoServiceImpl.java
@@ -48,9 +48,9 @@ public class PedidoServiceImpl implements PedidoService {
 
                     BigDecimal preco = produto.getPrecoUnitario();
                     Integer quantidade = dto.getQuantidade();
-                    BigDecimal multiplicar = new BigDecimal(quantidade);
+                    BigDecimal quantidadeBig = new BigDecimal(quantidade);
 
-                    BigDecimal total = preco.multiply(multiplicar);
+                    BigDecimal total = preco.multiply(quantidadeBig);
                 })
     }
 }
