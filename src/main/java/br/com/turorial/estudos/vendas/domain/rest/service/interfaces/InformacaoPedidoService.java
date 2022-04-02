0e9f741c35466c9213a6d5b70c3b00b1d6efe287
greg <gabriel.gregorio1987@gmail.com>
2022-04-02 20:32:41 -0300

    Criando assinatura de pegar a informação de um pedido completo

--- src/main/java/br/com/turorial/estudos/vendas/domain/rest/service/interfaces/InformacaoPedidoService.java
+++ src/main/java/br/com/turorial/estudos/vendas/domain/rest/service/interfaces/InformacaoPedidoService.java
@@ -1,4 +1,8 @@
 package br.com.turorial.estudos.vendas.domain.rest.service.interfaces;
 
+import br.com.turorial.estudos.vendas.domain.rest.dto.InformacoesPedidoDTO;
+
 public interface InformacaoPedidoService {
+
+    InformacoesPedidoDTO informacaoPedidoCompleto(Long id);
 }
