027a832f8c38171278887688b9450502af48a342
greg <gabriel.gregorio1987@gmail.com>
2022-04-02 21:05:45 -0300

    Importando o repositório de Pedido

--- src/main/java/br/com/turorial/estudos/vendas/domain/rest/service/impl/InformacaoPedidoImpl.java
+++ src/main/java/br/com/turorial/estudos/vendas/domain/rest/service/impl/InformacaoPedidoImpl.java
@@ -2,9 +2,11 @@ package br.com.turorial.estudos.vendas.domain.rest.service.impl;
 
 import br.com.turorial.estudos.vendas.domain.entity.ItemPedido;
 import br.com.turorial.estudos.vendas.domain.entity.Pedido;
+import br.com.turorial.estudos.vendas.domain.repository.PedidoRepository;
 import br.com.turorial.estudos.vendas.domain.rest.dto.InformacoesItensPedidoDTO;
 import br.com.turorial.estudos.vendas.domain.rest.dto.InformacoesPedidoDTO;
 import br.com.turorial.estudos.vendas.domain.rest.service.interfaces.InformacaoPedidoService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
@@ -14,7 +16,10 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class InformacaoPedidoImpl implements InformacaoPedidoService {
+
+    private final PedidoRepository pedidoRepository;
 
     @Override
     public InformacoesPedidoDTO informacaoPedidoCompleto(Long id) {
