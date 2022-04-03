a855cf1e83118f811f490786397af5c51286384d
greg <gabriel.gregorio1987@gmail.com>
2022-04-02 21:28:13 -0300

    Criando uma assinatura para atualizar o status

--- src/main/java/br/com/turorial/estudos/vendas/domain/rest/service/interfaces/InformacaoPedidoService.java
+++ src/main/java/br/com/turorial/estudos/vendas/domain/rest/service/interfaces/InformacaoPedidoService.java
@@ -1,8 +1,10 @@
 package br.com.turorial.estudos.vendas.domain.rest.service.interfaces;
 
+import br.com.turorial.estudos.vendas.domain.enums.StatusPedido;
 import br.com.turorial.estudos.vendas.domain.rest.dto.InformacoesPedidoDTO;
 
 public interface InformacaoPedidoService {
 
     InformacoesPedidoDTO informacaoPedidoCompleto(Long id);
+    void atualizarStatus(Long id, StatusPedido status);
 }
