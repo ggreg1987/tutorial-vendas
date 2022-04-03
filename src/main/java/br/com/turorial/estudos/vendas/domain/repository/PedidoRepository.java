9b624d2e1ec13988e7f6cd4264f753955f7465f5
greg <gabriel.gregorio1987@gmail.com>
2022-04-02 21:10:58 -0300

    Criando um HQL para pesquisar o pedido com os itens

--- src/main/java/br/com/turorial/estudos/vendas/domain/repository/PedidoRepository.java
+++ src/main/java/br/com/turorial/estudos/vendas/domain/repository/PedidoRepository.java
@@ -2,6 +2,13 @@ package br.com.turorial.estudos.vendas.domain.repository;
 
 import br.com.turorial.estudos.vendas.domain.entity.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface PedidoRepository extends JpaRepository<Pedido,Long> {
+
+    @Query("select p from Pedido p left join p.itens where p.id = :id")
+    Optional<Pedido> procurarPedido(@Param("id") Long id);
 }
