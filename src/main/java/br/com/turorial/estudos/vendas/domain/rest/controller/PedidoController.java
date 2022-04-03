99597390daca39b5970a511d3935b36c087522dc
greg <gabriel.gregorio1987@gmail.com>
2022-04-02 21:39:58 -0300

    Criando endpoint para atualizar status e adicionando as anotações do Lombok no StatusPedidoDTO

--- src/main/java/br/com/turorial/estudos/vendas/domain/rest/controller/PedidoController.java
+++ src/main/java/br/com/turorial/estudos/vendas/domain/rest/controller/PedidoController.java
@@ -1,8 +1,10 @@
 package br.com.turorial.estudos.vendas.domain.rest.controller;
 
 import br.com.turorial.estudos.vendas.domain.entity.Pedido;
+import br.com.turorial.estudos.vendas.domain.enums.StatusPedido;
 import br.com.turorial.estudos.vendas.domain.rest.dto.InformacoesPedidoDTO;
 import br.com.turorial.estudos.vendas.domain.rest.dto.PedidoDTO;
+import br.com.turorial.estudos.vendas.domain.rest.dto.StatusPedidoDTO;
 import br.com.turorial.estudos.vendas.domain.rest.service.interfaces.InformacaoPedidoService;
 import br.com.turorial.estudos.vendas.domain.rest.service.interfaces.PedidoService;
 import lombok.RequiredArgsConstructor;
@@ -28,5 +30,11 @@ public class PedidoController {
     @GetMapping("{id}")
     InformacoesPedidoDTO informacaoPedidoCompleto(@PathVariable Long id) {
         return infoService.informacaoPedidoCompleto(id);
+    }
+
+    @PatchMapping("{id}")
+    void atualizarStatus(@PathVariable Long id,@RequestBody StatusPedidoDTO dto) {
+        String status = dto.getStatusAtual();
+        infoService.atualizarStatus(id,StatusPedido.valueOf(status));
     }
 }
--- src/main/java/br/com/turorial/estudos/vendas/domain/rest/dto/StatusPedidoDTO.java
+++ src/main/java/br/com/turorial/estudos/vendas/domain/rest/dto/StatusPedidoDTO.java
@@ -1,5 +1,14 @@
 package br.com.turorial.estudos.vendas.domain.rest.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class StatusPedidoDTO {
 
     private String statusAtual;
