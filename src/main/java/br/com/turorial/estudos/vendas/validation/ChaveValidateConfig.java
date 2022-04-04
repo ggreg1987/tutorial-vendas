aaea327b171dc64a4ed43aae79d6c2181521dc68
greg <gabriel.gregorio1987@gmail.com>
2022-04-04 11:58:13 -0300

    Criando um bean do tipo MessageSource,onde será a fonte de msg do arquivo mensage.properties

--- src/main/java/br/com/turorial/estudos/vendas/validation/ChaveValidateConfig.java
+++ src/main/java/br/com/turorial/estudos/vendas/validation/ChaveValidateConfig.java
@@ -1,9 +1,14 @@
 package br.com.turorial.estudos.vendas.validation;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ChaveValidateConfig {
 
-    
+    @Bean
+    public MessageSource messageSource() {
+
+    }
 }
