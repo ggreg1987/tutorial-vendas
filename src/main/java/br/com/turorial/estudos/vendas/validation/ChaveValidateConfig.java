c91ec5de6cf7067c98bbf8a176fda15280dbd181
greg <gabriel.gregorio1987@gmail.com>
2022-04-04 12:11:46 -0300

    Criando uma instância do LocalValidatorFactoryBean

--- src/main/java/br/com/turorial/estudos/vendas/validation/ChaveValidateConfig.java
+++ src/main/java/br/com/turorial/estudos/vendas/validation/ChaveValidateConfig.java
@@ -22,6 +22,8 @@ public class ChaveValidateConfig {
 
     @Bean
     public LocalValidatorFactoryBean localValidatorFactoryBean() {
+        LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
+
 
     }
 }
