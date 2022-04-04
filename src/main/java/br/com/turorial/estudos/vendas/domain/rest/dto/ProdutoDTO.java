9c0c5ff743ea42dbb9a4b43ec5c1dc85b62a9af2
greg <gabriel.gregorio1987@gmail.com>
2022-04-04 11:08:47 -0300

    Adicionando chave do preço unitário do produto

--- src/main/java/br/com/turorial/estudos/vendas/domain/rest/dto/ProdutoDTO.java
+++ src/main/java/br/com/turorial/estudos/vendas/domain/rest/dto/ProdutoDTO.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Data
@@ -18,5 +19,7 @@ public class ProdutoDTO {
 
     @NotEmpty(message = "{campo.descricao.obrigatorio}")
     private String descricao;
+
+    @NotNull(message = "{campo.preco.obrigatorio}")
     private BigDecimal precoUnitario;
 }
