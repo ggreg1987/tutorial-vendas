c4f5d37423910030479d1b67a528773d454e892a
greg <gabriel.gregorio1987@gmail.com>
2022-04-04 12:00:21 -0300

    Instanciando ReloadableResourceBundleMessageSource por conta do arquivo ser .properties

--- src/main/java/br/com/turorial/estudos/vendas/validation/ChaveValidateConfig.java
+++ src/main/java/br/com/turorial/estudos/vendas/validation/ChaveValidateConfig.java
@@ -3,12 +3,13 @@ package br.com.turorial.estudos.vendas.validation;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @Configuration
 public class ChaveValidateConfig {
 
     @Bean
     public MessageSource messageSource() {
-
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
     }
 }
