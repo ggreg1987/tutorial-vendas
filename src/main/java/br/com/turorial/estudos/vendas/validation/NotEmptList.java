e439761e7c66199617836dc5388aff161ab0132c
greg <gabriel.gregorio1987@gmail.com>
2022-04-04 16:55:03 -0300

    Retornando para a notação NotEmptList e passando a class ConstraintValidationList para a notação Constraint

--- src/main/java/br/com/turorial/estudos/vendas/validation/NotEmptList.java
+++ src/main/java/br/com/turorial/estudos/vendas/validation/NotEmptList.java
@@ -1,5 +1,6 @@
 package br.com.turorial.estudos.vendas.validation;
 
+import javax.validation.Constraint;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -7,5 +8,6 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
+@Constraint(validatedBy = ConstraintValidationList.class)
 public @interface NotEmptList {
 }
