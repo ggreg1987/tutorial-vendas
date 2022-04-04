8db177c138dfb9343f5030a0c962529679d6e0e8
greg <gabriel.gregorio1987@gmail.com>
2022-04-04 16:46:13 -0300

    Implementando a interface ConstraintValidator

--- src/main/java/br/com/turorial/estudos/vendas/validation/ConstraintValidationList.java
+++ src/main/java/br/com/turorial/estudos/vendas/validation/ConstraintValidationList.java
@@ -1,4 +1,17 @@
 package br.com.turorial.estudos.vendas.validation;
 
-public class ConstraintValidationList {
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.lang.annotation.Annotation;
+
+public class ConstraintValidationList implements ConstraintValidator {
+    @Override
+    public void initialize(Annotation constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(Object value, ConstraintValidatorContext context) {
+        return false;
+    }
 }
