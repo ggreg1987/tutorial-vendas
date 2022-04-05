8722193d4e41f8632a43138897a47d99bf516363
greg <gabriel.gregorio1987@gmail.com>
2022-04-05 19:39:53 -0300

    Adicionando a anotação de configuração @EnableWebSecurity

--- src/main/java/br/com/turorial/estudos/vendas/config/security/SecurityConfig.java
+++ src/main/java/br/com/turorial/estudos/vendas/config/security/SecurityConfig.java
@@ -1,4 +1,7 @@
 package br.com.turorial.estudos.vendas.config.security;
 
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
 public class SecurityConfig {
 }
