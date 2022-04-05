98c670c2200fac412d2eedc9e8eddb02244f9f86
greg <gabriel.gregorio1987@gmail.com>
2022-04-05 19:44:34 -0300

    Adicionando o método AuthenticationManagerBuilder

--- src/main/java/br/com/turorial/estudos/vendas/config/security/SecurityConfig.java
+++ src/main/java/br/com/turorial/estudos/vendas/config/security/SecurityConfig.java
@@ -1,8 +1,14 @@
 package br.com.turorial.estudos.vendas.config.security;
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
 }
