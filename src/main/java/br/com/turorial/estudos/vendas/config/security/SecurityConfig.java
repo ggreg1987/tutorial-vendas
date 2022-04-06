fb3a04d57323d7fbeeab649cb0abf26ca8e8a5b4
greg <gabriel.gregorio1987@gmail.com>
2022-04-06 19:22:44 -0300

    Iniciando a configuração do HttpSecurity

--- src/main/java/br/com/turorial/estudos/vendas/config/security/SecurityConfig.java
+++ src/main/java/br/com/turorial/estudos/vendas/config/security/SecurityConfig.java
@@ -23,6 +23,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        
     }
 }
