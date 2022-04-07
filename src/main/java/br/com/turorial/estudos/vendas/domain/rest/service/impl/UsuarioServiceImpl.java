4ba4a1f30206e641017a0229233fbb4fb227f008
greg <gabriel.gregorio1987@gmail.com>
2022-04-07 14:36:44 -0300

    Fazendo a injeção do repositorio do Usuario

--- src/main/java/br/com/turorial/estudos/vendas/domain/rest/service/impl/UsuarioServiceImpl.java
+++ src/main/java/br/com/turorial/estudos/vendas/domain/rest/service/impl/UsuarioServiceImpl.java
@@ -1,5 +1,6 @@
 package br.com.turorial.estudos.vendas.domain.rest.service.impl;
 
+import br.com.turorial.estudos.vendas.domain.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
 public class UsuarioServiceImpl implements UserDetailsService {
 
     private final PasswordEncoder passwordEncoder;
+    private final UsuarioRepository usuarioRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
