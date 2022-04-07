66c19db3f8cc8a93249dbaaaba5225cf4bda6f32
greg <gabriel.gregorio1987@gmail.com>
2022-04-07 14:36:04 -0300

    Fazendo a injeção do PasswordEncoder

--- src/main/java/br/com/turorial/estudos/vendas/domain/rest/service/impl/UsuarioServiceImpl.java
+++ src/main/java/br/com/turorial/estudos/vendas/domain/rest/service/impl/UsuarioServiceImpl.java
@@ -1,12 +1,18 @@
 package br.com.turorial.estudos.vendas.domain.rest.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UsuarioServiceImpl implements UserDetailsService {
+
+    private final PasswordEncoder passwordEncoder;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return null;
