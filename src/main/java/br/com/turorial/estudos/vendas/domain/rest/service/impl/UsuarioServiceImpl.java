8c414c5a7864eef2d77bfc10dfd20ffd9dfb3f8b
greg <gabriel.gregorio1987@gmail.com>
2022-04-07 16:05:15 -0300

    Procurando pelo login do usuario

--- src/main/java/br/com/turorial/estudos/vendas/domain/rest/service/impl/UsuarioServiceImpl.java
+++ src/main/java/br/com/turorial/estudos/vendas/domain/rest/service/impl/UsuarioServiceImpl.java
@@ -17,6 +17,6 @@ public class UsuarioServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        usuarioRepository.findByLogin(username)
     }
 }
