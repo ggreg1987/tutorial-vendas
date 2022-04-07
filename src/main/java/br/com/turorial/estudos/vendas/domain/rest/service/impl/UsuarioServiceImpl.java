3a95fdf4fb97121bc349e6d2c28548e603bd791a
greg <gabriel.gregorio1987@gmail.com>
2022-04-07 14:09:07 -0300

    Implementando a interface UserDetailService e o método load

--- src/main/java/br/com/turorial/estudos/vendas/domain/rest/service/impl/UsuarioServiceImpl.java
+++ src/main/java/br/com/turorial/estudos/vendas/domain/rest/service/impl/UsuarioServiceImpl.java
@@ -1,7 +1,14 @@
 package br.com.turorial.estudos.vendas.domain.rest.service.impl;
 
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UsuarioServiceImpl {
+public class UsuarioServiceImpl implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
 }
