16e2672488a0c57bb88e24db95089a8325fcc57b
greg <gabriel.gregorio1987@gmail.com>
2022-04-07 22:42:42 -0300

    Criando o UsuarioController

---
+++ src/main/java/br/com/turorial/estudos/vendas/domain/rest/controller/UsuarioController.java
@@ -0,0 +1,12 @@
+package br.com.turorial.estudos.vendas.domain.rest.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/usuario")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+}
