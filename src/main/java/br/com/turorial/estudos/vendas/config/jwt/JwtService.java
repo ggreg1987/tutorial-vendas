11056806bf6a05e7f8702cf0f96392c380cccdbd
greg <gabriel.gregorio1987@gmail.com>
2022-04-10 18:29:28 -0300

    Passando a chave para os atributos

--- src/main/java/br/com/turorial/estudos/vendas/config/jwt/JwtService.java
+++ src/main/java/br/com/turorial/estudos/vendas/config/jwt/JwtService.java
@@ -1,10 +1,14 @@
 package br.com.turorial.estudos.vendas.config.jwt;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
 
+    @Value("{security.jwt.expiracao}")
     private String expiracao;
+
+    @Value("{security.jwt.chave-assinatura}")
     private String assinatura;
 }
