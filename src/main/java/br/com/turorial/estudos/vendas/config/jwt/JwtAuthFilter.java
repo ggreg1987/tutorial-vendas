a9c9737123729c879b684bd4f9902dab0864a701
greg <gabriel.gregorio1987@gmail.com>
2022-04-12 03:16:45 -0300

    Fazendo a dependência via construtor do UsuarioServiceImpl e JwtService

--- src/main/java/br/com/turorial/estudos/vendas/config/jwt/JwtAuthFilter.java
+++ src/main/java/br/com/turorial/estudos/vendas/config/jwt/JwtAuthFilter.java
@@ -1,5 +1,6 @@
 package br.com.turorial.estudos.vendas.config.jwt;
 
+import br.com.turorial.estudos.vendas.domain.rest.service.impl.UsuarioServiceImpl;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -10,7 +11,13 @@ import java.io.IOException;
 
 public class JwtAuthFilter extends OncePerRequestFilter {
 
+    private JwtService jwtService;
+    private UsuarioServiceImpl usuarioService;
 
+    public JwtAuthFilter(JwtService jwtService, UsuarioServiceImpl usuarioService) {
+        this.jwtService = jwtService;
+        this.usuarioService = usuarioService;
+    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
