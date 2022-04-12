f77910e42752ac98c1ad4e663c1d2a30803772be
greg <gabriel.gregorio1987@gmail.com>
2022-04-12 03:15:29 -0300

    extendendo a class OncePerRequestFilter e imeplementando seu método doFilterInternal

--- src/main/java/br/com/turorial/estudos/vendas/config/jwt/JwtAuthFilter.java
+++ src/main/java/br/com/turorial/estudos/vendas/config/jwt/JwtAuthFilter.java
@@ -1,4 +1,22 @@
 package br.com.turorial.estudos.vendas.config.jwt;
 
-public class JwtAuthFilter {
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+                                    throws ServletException, IOException {
+
+    }
 }
