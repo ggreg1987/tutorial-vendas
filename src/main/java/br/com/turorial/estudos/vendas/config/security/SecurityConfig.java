d09a11eec864ea58bd96a488ecdae17c461ee59e
greg <gabriel.gregorio1987@gmail.com>
2022-04-13 16:01:46 -0300

    Definindo o filtro customizado para ser executado antes do filtro do spring security

--- src/main/java/br/com/turorial/estudos/vendas/config/security/SecurityConfig.java
+++ src/main/java/br/com/turorial/estudos/vendas/config/security/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @EnableWebSecurity
@@ -56,8 +57,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         .permitAll()
                 .anyRequest().authenticated()
                 .and()
-                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                    .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
 
 
     }
