9162bec2cf7ad2e1f063308b809099fcadee0b90
greg <gabriel.gregorio1987@gmail.com>
2022-04-13 15:52:42 -0300

    Criando um Bean para representar a class JwtAuthFilter

--- src/main/java/br/com/turorial/estudos/vendas/config/security/SecurityConfig.java
+++ src/main/java/br/com/turorial/estudos/vendas/config/security/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -19,6 +20,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UsuarioServiceImpl usuarioService;
     private final JwtAuthFilter jwtAuthFilter;
+
+    @Bean
+    public OncePerRequestFilter jwtFilter() {
+
+    }
 
 
     @Bean
