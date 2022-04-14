0d964b7522d7baa788caa1dc219fe4c424440928
greg <gabriel.gregorio1987@gmail.com>
2022-04-14 13:04:37 -0300

    Criando um método para contato

--- src/main/java/br/com/turorial/estudos/vendas/config/swagger/SwaggerConfig.java
+++ src/main/java/br/com/turorial/estudos/vendas/config/swagger/SwaggerConfig.java
@@ -1,9 +1,14 @@
 package br.com.turorial.estudos.vendas.config.swagger;
 
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.Contact;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
+    private Contact contact() {
+        
+    }
 }
