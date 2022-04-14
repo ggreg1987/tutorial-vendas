a7017a659049c68836431cc917f062d59e946fbe
greg <gabriel.gregorio1987@gmail.com>
2022-04-14 13:38:57 -0300

    Criando um bean do Docket

--- src/main/java/br/com/turorial/estudos/vendas/config/swagger/SwaggerConfig.java
+++ src/main/java/br/com/turorial/estudos/vendas/config/swagger/SwaggerConfig.java
@@ -1,9 +1,11 @@
 package br.com.turorial.estudos.vendas.config.swagger;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
@@ -23,5 +25,10 @@ public class SwaggerConfig {
                 .version("1.0")
                 .contact(contact())
                 .build();
+    }
+
+    @Bean
+    public Docket docket() {
+        
     }
 }
