f4620eb4b61a22ff8d263a4985179a3e3e1793a9
greg <gabriel.gregorio1987@gmail.com>
2022-04-15 13:05:13 -0300

    Passando a chave para o atributo login

--- src/main/java/br/com/turorial/estudos/vendas/domain/rest/dto/CredenciaisDTO.java
+++ src/main/java/br/com/turorial/estudos/vendas/domain/rest/dto/CredenciaisDTO.java
@@ -3,10 +3,13 @@ package br.com.turorial.estudos.vendas.domain.rest.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 @NoArgsConstructor
 public class CredenciaisDTO {
 
+    @NotEmpty(message = "{campo.login.obrigatorio}")
     private String login;
     private String senha;
 }
