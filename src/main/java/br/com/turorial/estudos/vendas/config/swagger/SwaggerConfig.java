5cd72fb3b6e97855248cfba90baee9a2f609dcdb
greg <gabriel.gregorio1987@gmail.com>
2022-04-14 23:15:31 -0300

    Criando uma instância de AuthorizationScope

--- src/main/java/br/com/turorial/estudos/vendas/config/swagger/SwaggerConfig.java
+++ src/main/java/br/com/turorial/estudos/vendas/config/swagger/SwaggerConfig.java
@@ -6,10 +6,7 @@ import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.Contact;
-import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -53,6 +50,6 @@ public class SwaggerConfig {
     }
 
     private List<SecurityReference> references() {
-        
+        AuthorizationScope authorizationScope = new AuthorizationScope()
     }
 }
